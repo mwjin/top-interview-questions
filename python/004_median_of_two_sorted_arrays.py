7a8003cd5fa9a23933dddf2207d0c1e69edd3140
Minwoo Jeong <jeongmwj@gmail.com>
2022-08-11 13:06:13 +0900

    Solve `4. Median of Two Sorted Arrays` with Kth element

--- python/004_median_of_two_sorted_arrays.py
+++ python/004_median_of_two_sorted_arrays.py
@@ -2,31 +2,46 @@ from typing import List
 
 
 class Solution:
+    MAX_VALUE = 1000001
+
     def findMedianSortedArrays(
         self, nums1: List[int], nums2: List[int]
     ) -> float:
-        def merge(l1: List[int], l2: List[int]) -> List[int]:
-            result = []
-            i = j = 0
+        self.nums1 = nums1
+        self.nums2 = nums2
+        left = (len(nums1) + len(nums2) + 1) // 2
+        right = (len(nums1) + len(nums2) + 2) // 2
+        return (
+            self.findKthElement(left, 0, 0) + self.findKthElement(right, 0, 0)
+        ) / 2
 
-            while i < len(l1) and j < len(l2):
-                if l1[i] < l2[j]:
-                    result.append(l1[i])
-                    i += 1
-                else:
-                    result.append(l2[j])
-                    j += 1
+    def findKthElement(self, k: int, nums1_offset: int, nums2_offset: int):
+        if nums1_offset >= len(self.nums1):
+            return self.nums2[nums2_offset + k - 1]
+        if nums2_offset >= len(self.nums2):
+            return self.nums1[nums1_offset + k - 1]
+        if k == 1:
+            return min(self.nums1[nums1_offset], self.nums2[nums2_offset])
 
-            if i < len(l1):
-                result += l1[i:]
-            elif j < len(l2):
-                result += l2[j:]
-            return result
+        nums1_idx = nums1_offset + k // 2 - 1
+        nums2_idx = nums2_offset + k // 2 - 1
 
-        def median(l: List[int]) -> float:
-            mid = len(l) // 2
-            return (
-                float(l[mid]) if len(l) % 2 == 1 else (l[mid - 1] + l[mid]) / 2
+        nums1_val = (
+            self.nums1[nums1_idx]
+            if nums1_idx < len(self.nums1)
+            else self.MAX_VALUE
+        )
+        nums2_val = (
+            self.nums2[nums2_idx]
+            if nums2_idx < len(self.nums2)
+            else self.MAX_VALUE
+        )
+
+        if nums1_val < nums2_val:
+            return self.findKthElement(
+                k - k // 2, nums1_offset + k // 2, nums2_offset
             )
-
-        return median(merge(nums1, nums2))
+        else:
+            return self.findKthElement(
+                k - k // 2, nums1_offset, nums2_offset + k // 2
+            )
