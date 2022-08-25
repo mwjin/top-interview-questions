ab181658cd2350c2c9583c7f8b44f571083d2044
Minwoo Jeong <jeongmwj@gmail.com>
2022-08-25 10:30:16 +0900

    Improve the solution

--- python/146_lru_cache.py
+++ python/146_lru_cache.py
@@ -1,33 +1,24 @@
-from typing import Optional
-from __future__ import annotations
-
-
 class ListNode:
-    def __init__(
-        self,
-        val: int = -1,
-        prev: Optional[ListNode] = None,
-        next: Optional[ListNode] = None,
-    ) -> None:
-        self.val = val
-        self.prev = prev
-        self.next = next
+    def __init__(self, key: int = -1, value: int = -1) -> None:
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
 
 
 class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.num_keys = 0
-        self.key_list_head = ListNode()
-        self.key_list_tail = ListNode(prev=self.key_list_head)
-        self.key_list_head.next = self.key_list_tail
         self.key_to_node = {}
-        self.key_to_value = {}
+        self.list_head = ListNode()
+        self.list_tail = ListNode()
+        self.list_head.next = self.list_tail
+        self.list_tail.prev = self.list_head
 
     def get(self, key: int) -> int:
-        if key in self.key_to_value:
+        if key in self.key_to_node:
             self._move_key_to_most_recently_used(key)
-            return self.key_to_value[key]
+            return self.key_to_node[key].value
         return -1
 
     def _move_key_to_most_recently_used(self, key: int):
@@ -37,34 +28,29 @@ class LRUCache:
         self._insert_into_last(node)
 
     def _insert_into_last(self, node: ListNode):
-        node.prev, node.next = self.key_list_tail.prev, self.key_list_tail
-        self.key_list_tail.prev.next = node
-        self.key_list_tail.prev = node
+        node.prev, node.next = self.list_tail.prev, self.list_tail
+        self.list_tail.prev.next = node
+        self.list_tail.prev = node
 
     def put(self, key: int, value: int) -> None:
-        if key in self.key_to_value:
+        if key in self.key_to_node:
             self._move_key_to_most_recently_used(key)
+            self.key_to_node[key].value = value
         else:
-            self._append_new_key(key)
-            self._evict_least_recently_used_if_exceeds()
+            self._append_new_node(key, value)
+            self._evict_lru_if_exceeds()
 
-        self.key_to_value[key] = value
+    def _append_new_node(self, key: int, value: int):
+        new_node = ListNode(key, value)
+        self._insert_into_last(new_node)
+        self.key_to_node[key] = new_node
 
-    def _append_new_key(self, key: int):
-        key_node = ListNode(key)
-        self._insert_into_last(key_node)
-        self.key_to_node[key] = key_node
-        self.num_keys += 1
-
-    def _evict_least_recently_used_if_exceeds(self):
-        if self.num_keys > self.capacity:
-            lru_key = self.key_list_head.next.val
-            lru_node = self.key_to_node[lru_key]
+    def _evict_lru_if_exceeds(self):
+        if len(self.key_to_node) > self.capacity:
+            lru_node = self.list_head.next
             lru_node.prev.next = lru_node.next
             lru_node.next.prev = lru_node.prev
-            del self.key_to_node[lru_key]
-            del self.key_to_value[lru_key]
-            self.num_keys -= 1
+            del self.key_to_node[lru_node.key]
 
 
 # Your LRUCache object will be instantiated and called as such:
