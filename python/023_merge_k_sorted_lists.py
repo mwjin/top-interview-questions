9ad95ce8b01648bc8907154e9f6176b66c74ef73
Minwoo Jeong <jeongmwj@gmail.com>
2022-08-30 09:52:07 +0900

    Solve with divide and conquer approach

--- python/023_merge_k_sorted_lists.py
+++ python/023_merge_k_sorted_lists.py
@@ -13,18 +13,27 @@ class Solution:
     def mergeKLists(
         self, lists: List[Optional[ListNode]]
     ) -> Optional[ListNode]:
+        def mergeTwoLists(list1: ListNode, list2: ListNode) -> ListNode:
+            root = head = ListNode()
+            while list1 and list2:
+                if list1.val < list2.val:
+                    head.next, list1 = list1, list1.next
+                else:
+                    head.next, list2 = list2, list2.next
+                head = head.next
+
+            remain = list1 if list1 else list2
+            head.next = remain
+            return root.next
+
         if not lists:
             return None
-
-        values = []
-        for list in lists:
-            while list:
-                heappush(values, list.val)
-                list = list.next
-
-        root = head = ListNode()
-        while values:
-            head.next = ListNode(heappop(values))
-            head = head.next
-
-        return root.next
+        elif len(lists) == 1:
+            return lists[0]
+        elif len(lists) == 2:
+            return mergeTwoLists(lists[0], lists[1])
+        else:
+            mid = len(lists) // 2
+            return mergeTwoLists(
+                self.mergeKLists(lists[:mid]), self.mergeKLists(lists[mid:])
+            )
