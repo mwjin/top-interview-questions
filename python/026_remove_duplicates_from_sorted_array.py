c4645184a6a8c7cc13973000f80aea2bd036772a
Minwoo Jeong <jeongmwj@gmail.com>
2022-08-31 09:03:11 +0900

    Improve the solution by not using `enumerate`

--- python/026_remove_duplicates_from_sorted_array.py
+++ python/026_remove_duplicates_from_sorted_array.py
@@ -5,8 +5,8 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         lastValIdx = 0
 
-        for i, n in enumerate(nums):
-            if n > nums[lastValIdx]:
+        for i in range(1, len(nums)):
+            if nums[i] > nums[lastValIdx]:
                 nums[lastValIdx + 1], nums[i] = nums[i], nums[lastValIdx + 1]
                 lastValIdx += 1
 
