c6037b2eadbecd68e5e8d7ca466eb66b8b2013a6
Minwoo Jeong <jeongmwj@gmail.com>
2022-09-01 09:52:45 +0900

    Solve with clearer solution
    Use shift operators

--- python/029_divide_two_integers.py
+++ python/029_divide_two_integers.py
@@ -3,33 +3,30 @@ class Solution:
         def trim_result(result: int) -> int:
             int_min = -(2 ** 31)
             int_max = 2 ** 31 - 1
-            return (
-                min(result, int_max) if result >= 0 else max(int_min, result)
-            )
+            return max(min(result, int_max), int_min)
 
         if divisor == 1:
             return trim_result(dividend)
         if divisor == -1:
             return trim_result(-dividend)
 
-        result = 0
-
-        sign = 1 if dividend ^ divisor >= 0 else -1
+        positive = (dividend < 0) == (divisor < 0)
         abs_dividend = abs(dividend)
         abs_divisor = abs(divisor)
+
+        result = 0
 
         while abs_dividend >= abs_divisor:
             q = 1
             temp_divisor = abs_divisor
-            while temp_divisor < abs_dividend:
+
+            while abs_dividend >= temp_divisor:
+                abs_dividend -= temp_divisor
+                result += q
                 temp_divisor <<= 1
                 q <<= 1
 
-            if temp_divisor > abs_dividend:
-                temp_divisor >>= 1
-                q >>= 1
+        return trim_result(result if positive else -result)
 
-            abs_dividend -= temp_divisor
-            result += q
 
-        return trim_result(result if sign == 1 else -result)
+print(Solution().divide(10, 3))
