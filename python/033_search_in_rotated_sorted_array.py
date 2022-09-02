3b4b575ccca76f3d24a79301bced2d6de53f1123
Minwoo Jeong <jeongmwj@gmail.com>
2022-09-02 10:01:39 +0900

    Solve with more efficient solution

--- python/033_search_in_rotated_sorted_array.py
+++ python/033_search_in_rotated_sorted_array.py
@@ -3,45 +3,31 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if len(nums) == 1:
-            return 0 if nums[0] == target else -1
+        left, right = 0, len(nums) - 1
 
-        self.nums = nums
-        start_idx = self.find_start_idx()
-        end_idx = start_idx - 1
-        if end_idx < 0:
-            end_idx += len(nums)
-        self.pivot = len(nums) - start_idx
-
-        left = self.get_org_idx(start_idx)
-        right = self.get_org_idx(end_idx)
-        mid = self.get_pivot_idx((left + right) // 2)
-
-        while left <= right and nums[mid] != target:
-            if nums[mid] < target:
-                left = self.get_org_idx(mid) + 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[right]:
+                left = mid + 1
             else:
-                right = self.get_org_idx(mid) - 1
-            mid = self.get_pivot_idx((left + right) // 2)
+                right = mid
 
-        return mid if nums[mid] == target else -1
+        rotate = left
+        left, right = 0, len(nums) - 1
 
-    def find_start_idx(self):
-        for i in range(1, len(self.nums)):
-            if self.nums[i - 1] > self.nums[i]:
-                return i
-        return 0
+        while left <= right:
+            mid = (left + right) // 2
+            rotated_mid = (mid + rotate) % len(nums)
+            if nums[rotated_mid] > target:
+                right = mid - 1
+            elif nums[rotated_mid] < target:
+                left = mid + 1
+            else:
+                return rotated_mid
 
-    def get_pivot_idx(self, org_idx: int):
-        result = org_idx - self.pivot
-        if result < 0:
-            result += len(self.nums)
-        return result
-
-    def get_org_idx(self, pivot_idx: int):
-        return (pivot_idx + self.pivot) % len(self.nums)
+        return -1
 
 
-print(Solution().search([1, 3], 1))
-print(Solution().search([4, 5, 6, 7, 0, 1, 2], 0))
-print(Solution().search([4, 5, 6, 7, 0, 1, 2], 3))
+print(Solution().search([1, 3], 0))
+print(Solution().search([3, 1, 2], 0))
+print(Solution().search([4, 2, 6, 7, 0, 1, 2], 0))
