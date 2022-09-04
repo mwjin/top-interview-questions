6227fbbaf0f26332e291badce5ce3e24f14e626b
Minwoo Jeong <jeongmwj@gmail.com>
2022-09-04 10:50:56 +0900

    Improve the solution

--- python/034_find_first_and_last_position_of_element_in_sorted_array.py
+++ python/034_find_first_and_last_position_of_element_in_sorted_array.py
@@ -3,37 +3,34 @@ from typing import List
 
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def lsearch(nums: List[int], target: int) -> int:
-            left, right = 0, len(nums) - 1
-            result = -1
-
-            while left <= right:
-                mid = left + (right - left) // 2
-                if nums[mid] == target:
-                    result = mid
-                if nums[mid] >= target:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-
+        result = [-1, -1]
+        if not nums:
             return result
 
-        def rsearch(nums: List[int], target: int) -> int:
-            left, right = 0, len(nums) - 1
-            result = -1
+        left, right = 0, len(nums) - 1
 
-            while left <= right:
-                mid = left + (right - left) // 2
-                if nums[mid] == target:
-                    result = mid
-                if nums[mid] > target:
-                    right = mid - 1
-                else:
-                    left = mid + 1
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] >= target:
+                right = mid
+            else:
+                left = mid + 1
 
+        if nums[left] != target:
             return result
 
-        return [lsearch(nums, target), rsearch(nums, target)]
+        result[0] = left
+        right = len(nums) - 1
+
+        while left < right:
+            mid = right - (right - left) // 2
+            if nums[mid] <= target:
+                left = mid
+            else:
+                right = mid - 1
+
+        result[1] = right if nums[right] == target else result[0]
+        return result
 
 
 print(Solution().searchRange([5, 7, 7, 8, 8, 10], 8))
