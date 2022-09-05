f7ed77af30cbfb70c75f1eb12abda1b23567953f
Minwoo Jeong <jeongmwj@gmail.com>
2022-09-05 09:37:37 +0900

    Solve with smarter solution

--- python/036_valid_sudoku.py
+++ python/036_valid_sudoku.py
@@ -3,42 +3,23 @@ from typing import List
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        def isValidRow(row_idx: int):
-            digit_set = set()
-            for col_idx in range(9):
-                cell = board[row_idx][col_idx]
-                if cell in digit_set and cell.isdigit():
-                    return False
-                else:
-                    digit_set.add(cell)
-            return True
-
-        def isValidCol(col_idx: int):
-            digit_set = set()
-            for row_idx in range(9):
-                cell = board[row_idx][col_idx]
-                if cell in digit_set and cell.isdigit():
-                    return False
-                else:
-                    digit_set.add(cell)
-            return True
-
-        def isValidSubBox(row_idx: int, col_idx: int):
-            digit_set = set()
-            for i in range(row_idx, row_idx + 3):
-                for j in range(col_idx, col_idx + 3):
-                    cell = board[i][j]
-                    if cell in digit_set and cell.isdigit():
-                        return False
-                    else:
-                        digit_set.add(cell)
-            return True
+        cell_set = set()
 
         for i in range(9):
-            if not isValidRow(i) or not isValidCol(i):
-                return False
-        for i in range(0, 9, 3):
-            for j in range(0, 9, 3):
-                if not isValidSubBox(i, j):
-                    return False
+            for j in range(9):
+                if board[i][j] != ".":
+                    row_label = f"{i}({board[i][j]})"
+                    col_label = f"({board[i][j]}){j}"
+                    sub_box_label = f"{i // 3}({board[i][j]}){j // 3}"
+
+                    if (
+                        row_label in cell_set
+                        or col_label in cell_set
+                        or sub_box_label in cell_set
+                    ):
+                        return False
+
+                    cell_set.add(row_label)
+                    cell_set.add(col_label)
+                    cell_set.add(sub_box_label)
         return True
