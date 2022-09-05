ddc317d0d731d2511166dfd3f6f0a2bac6d327e5
Minwoo Jeong <jeongmwj@gmail.com>
2022-09-06 08:38:58 +0900

    Solve with itertools

--- python/038_count_and_say.py
+++ python/038_count_and_say.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 class Solution:
     def countAndSay(self, n: int) -> str:
         if n == 1:
@@ -7,21 +10,13 @@ class Solution:
 
 
 def convertDigitStr(digitStr: str) -> str:
-    currDigit = digitStr[0]
-    digitCnt = 1
-    cntResults = []
-
-    for i in range(1, len(digitStr)):
-        if digitStr[i] == currDigit:
-            digitCnt += 1
-        else:
-            cntResults.append((digitCnt, currDigit))
-            currDigit = digitStr[i]
-            digitCnt = 1
-
-    cntResults.append((digitCnt, currDigit))
-    return "".join(map(lambda result: f"{result[0]}{result[1]}", cntResults))
+    group_length = lambda group: len(list(group))
+    return "".join(
+        [
+            f"{group_length(group)}{digit}"
+            for digit, group in itertools.groupby(digitStr)
+        ]
+    )
 
 
 print(convertDigitStr("3322251"))
-print(Solution().countAndSay(4))
