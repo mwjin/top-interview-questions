3c76292d2afe1db037d51da4799f2f0cf8c3104b
Minwoo Jeong <jeongmwj@gmail.com>
2022-09-07 09:16:31 +0900

    Solve with two pointer
    Simple but slow than the previous one

--- python/042_trapping_rain_water.py
+++ python/042_trapping_rain_water.py
@@ -3,27 +3,22 @@ from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
+        left, right = 0, len(height) - 1
+        maxLeft = maxRight = 0
         result = 0
-        currHeight = height[0]
-        currWater = 0
 
-        for i in range(1, len(height)):
-            if height[i] >= currHeight:
-                result += currWater
-                currHeight = height[i]
-                currWater = 0
+        while left <= right:
+            if height[left] <= height[right]:
+                if height[left] >= maxLeft:
+                    maxLeft = height[left]
+                else:
+                    result += maxLeft - height[left]
+                left += 1
             else:
-                currWater += currHeight - height[i]
-
-        currHeight = height[-1]
-        currWater = 0
-
-        for i in range(len(height) - 1, -1, -1):
-            if height[i] > currHeight:
-                result += currWater
-                currHeight = height[i]
-                currWater = 0
-            else:
-                currWater += currHeight - height[i]
+                if height[right] >= maxRight:
+                    maxRight = height[right]
+                else:
+                    result += maxRight - height[right]
+                right -= 1
 
         return result
