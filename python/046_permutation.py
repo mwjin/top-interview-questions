1b29c06a310151a8465f9071c1677cf67623fb52
Minwoo Jeong <jeongmwj@gmail.com>
2022-09-09 08:29:30 +0900

    Solve with `itertools`
    Less efficient than the previous one

--- python/046_permutation.py
+++ python/046_permutation.py
@@ -1,22 +1,10 @@
+import itertools
 from typing import List
 
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        result = []
-
-        def dfs(path=[]):
-            if len(path) == len(nums):
-                result.append(path)
-                return
-
-            for n in nums:
-                if n not in path:
-                    new_path = [*path, n]
-                    dfs(new_path)
-
-        dfs()
-        return result
+        return list(itertools.permutations(nums))
 
 
 print(Solution().permute([1, 2, 3]))
