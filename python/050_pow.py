a147c6e19795e587708a871e66b5f63f030642c0
Minwoo Jeong <jeongmwj@gmail.com>
2022-09-15 07:52:55 +0900

    Solve with iterative approach

--- python/050_pow.py
+++ python/050_pow.py
@@ -1,17 +1,18 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n == 0:
-            return 1
-
-        result = x
-        p = 2
+        result = 1
         abs_n = abs(n)
 
-        while p <= abs_n:
-            result *= result
-            p *= 2
+        while abs_n > 0:
+            temp = x
+            p = 2
 
-        result *= self.myPow(x, abs_n - p // 2)
+            while p <= abs_n:
+                temp *= temp
+                p *= 2
+
+            result *= temp
+            abs_n -= p // 2
 
         if n < 0:
             result = 1 / result
