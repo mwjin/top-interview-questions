1361b45ad59d1c3c41d32d56c9859d8cb030371b
Minwoo Jeong <jeongmwj@gmail.com>
2022-09-21 09:52:56 +0900

    Solve without sets but more complex

--- python/073_set_matrix_zeros.py
+++ python/073_set_matrix_zeros.py
@@ -6,19 +6,27 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        zero_row_set = set()
-        zero_col_set = set()
+        col0_is_zero = False
+        row_size, col_size = len(matrix), len(matrix[0])
 
-        for row_idx in range(len(matrix)):
-            for col_idx in range(len(matrix[row_idx])):
+        for row in matrix:
+            if row[0] == 0:
+                col0_is_zero = True
+                break
+
+        for row_idx in range(row_size):
+            for col_idx in range(1, col_size):
                 if matrix[row_idx][col_idx] == 0:
-                    zero_row_set.add(row_idx)
-                    zero_col_set.add(col_idx)
+                    matrix[row_idx][0] = matrix[0][col_idx] = 0
 
-        for row_idx in range(len(matrix)):
-            for col_idx in range(len(matrix[row_idx])):
-                if row_idx in zero_row_set or col_idx in zero_col_set:
+        for row_idx in range(row_size - 1, -1, -1):
+            for col_idx in range(col_size - 1, 0, -1):
+                if matrix[row_idx][0] == 0 or matrix[0][col_idx] == 0:
                     matrix[row_idx][col_idx] = 0
+
+        if col0_is_zero:
+            for row in matrix:
+                row[0] = 0
 
         print(matrix)
 
