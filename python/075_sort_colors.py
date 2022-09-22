f3bd4f8920bf017fc919014b1bb62d76cd6b6a75
Minwoo Jeong <jeongmwj@gmail.com>
2022-09-22 09:32:25 +0900

    Solve as the dutch partitioning problem

--- python/075_sort_colors.py
+++ python/075_sort_colors.py
@@ -1,4 +1,3 @@
-from collections import Counter
 from typing import List
 
 
@@ -7,13 +6,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        cnt = Counter(nums)
-        offset = 0
+        red, white, blue = 0, 0, len(nums) - 1
 
-        for n in range(3):
-            for i in range(offset, offset + cnt[n]):
-                nums[i] = n
-            offset += cnt[n]
+        while white <= blue:
+            if nums[white] == 0:  # Red
+                nums[red], nums[white] = nums[white], nums[red]
+                red += 1
+                white += 1
+            elif nums[white] == 1:  # White
+                white += 1
+            else:
+                nums[white], nums[blue] = nums[blue], nums[white]
+                blue -= 1
 
 
 Solution().sortColors([2, 0, 2, 1, 1, 0])
+Solution().sortColors([2, 0, 1])
