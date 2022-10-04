285f939385c084416d3f3605c670b457a1334d9b
Minwoo Jeong <jeongmwj@gmail.com>
2022-10-04 09:26:51 +0900

    Improve the solution to use constant space

--- python/091_decode_ways.py
+++ python/091_decode_ways.py
@@ -1,14 +1,15 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        dp = [-1 for _ in range(len(s) + 1)]
-        dp[-1] = 1
+        prevPrev = -1
+        prev = 1
 
         for i in range(len(s) - 1, -1, -1):
             if s[i] == "0":
-                dp[i] = 0
+                curr = 0
             else:
-                dp[i] = dp[i + 1]
+                curr = prev
                 if i < len(s) - 1 and int(s[i : i + 2]) <= 26:
-                    dp[i] += dp[i + 2]
+                    curr += prevPrev
+            prevPrev, prev = prev, curr
 
-        return dp[0]
+        return curr
