d4796c334f70ddd423d6c650c75d68738dee3d91
Minwoo Jeong <jeongmwj@gmail.com>
2022-10-04 09:21:49 +0900

    Improve the solution with iterative approach

--- python/091_decode_ways.py
+++ python/091_decode_ways.py
@@ -1,24 +1,14 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        self.s = s
-        self.dp = [-1 for _ in range(len(s))]
-        return self.numDecodingsInternal(0)
+        dp = [-1 for _ in range(len(s) + 1)]
+        dp[-1] = 1
 
-    def numDecodingsInternal(self, idx: int) -> int:
-        if idx == len(self.s):
-            return 1
-        if self.s[idx] == "0":
-            return 0
-        if idx == len(self.s) - 1:
-            return 1
-        if self.dp[idx] != -1:
-            return self.dp[idx]
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == "0":
+                dp[i] = 0
+            else:
+                dp[i] = dp[i + 1]
+                if i < len(s) - 1 and int(s[i : i + 2]) <= 26:
+                    dp[i] += dp[i + 2]
 
-        result = self.numDecodingsInternal(idx + 1)
-
-        if int(self.s[idx : idx + 2]) <= 26:
-            result += self.numDecodingsInternal(idx + 2)
-
-        self.dp[idx] = result
-
-        return result
+        return dp[0]
