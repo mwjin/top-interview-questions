55e6c0a3bd5e30d6a17466bc4d565259fc725b90
Minwoo Jeong <jeongmwj@gmail.com>
2022-10-05 09:39:29 +0900

    Solve `098. Validate Binary Search Tree`

---
+++ python/098_validate_binary_search_tree.py
@@ -0,0 +1,27 @@
+from typing import Optional, List
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        inorderList = self.inorderTraversal(root)
+
+        for i in range(1, len(inorderList)):
+            if inorderList[i - 1] >= inorderList[i]:
+                return False
+        return True
+
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        return [
+            *self.inorderTraversal(root.left),
+            root.val,
+            *self.inorderTraversal(root.right),
+        ]
