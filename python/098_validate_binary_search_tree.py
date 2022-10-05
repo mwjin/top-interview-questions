dfa863deff3666504e4371dc6f4dc06311fb7632
Minwoo Jeong <jeongmwj@gmail.com>
2022-10-05 09:49:50 +0900

    Improve the performance a little

--- python/098_validate_binary_search_tree.py
+++ python/098_validate_binary_search_tree.py
@@ -20,8 +20,7 @@ class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
             return []
-        return [
-            *self.inorderTraversal(root.left),
-            root.val,
-            *self.inorderTraversal(root.right),
-        ]
+        result = self.inorderTraversal(root.left)
+        result.append(root.val)
+        result += self.inorderTraversal(root.right)
+        return result
