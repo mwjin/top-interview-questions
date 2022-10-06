4c240e08a42159ddf6f9bed5a136fe88f3b26b72
Minwoo Jeong <jeongmwj@gmail.com>
2022-10-06 09:34:31 +0900

    Solve with interative approach

--- python/094_binary_tree_inorder_traversal.py
+++ python/094_binary_tree_inorder_traversal.py
@@ -11,10 +11,15 @@ class TreeNode:
 
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
-            return []
-        return [
-            *self.inorderTraversal(root.left),
-            root.val,
-            *self.inorderTraversal(root.right),
-        ]
+        result = []
+        stack = []
+        curr = root
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            result.append(curr.val)
+            curr = curr.right
+
+        return result
