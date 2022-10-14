071be0ea7b92156579fbb8f790f2513253c6bfc8
Minwoo Jeong <jeongmwj@gmail.com>
2022-10-14 09:11:25 +0900

    Shorten the solution

--- python/118_pascals_triangle.py
+++ python/118_pascals_triangle.py
@@ -3,18 +3,10 @@ from typing import List
 
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        result = []
-        for n in range(1, numRows + 1):
-            if n == 1:
-                result.append([1])
-            elif n == 2:
-                result.append([1, 1])
-            else:
-                prev = result[-1]
-                curr = [1]
-                for i in range(1, len(prev)):
-                    curr.append(prev[i - 1] + prev[i])
-                curr.append(1)
-                result.append(curr)
+        result = [[1 for _ in range(n)] for n in range(1, numRows + 1)]
+
+        for i in range(2, len(result)):
+            for j in range(1, i):
+                result[i][j] = result[i - 1][j - 1] + result[i - 1][j]
 
         return result
