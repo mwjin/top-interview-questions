6ac574e0d3fbdc019046a2d89b41a70e89624458
Minwoo Jeong <jeongmwj@gmail.com>
2022-10-20 09:49:29 +0900

    Solve with O(n) solution
    but slower than the solution with sorting

--- python/128_longest_consecutive_sequence.py
+++ python/128_longest_consecutive_sequence.py
@@ -3,21 +3,18 @@ from typing import List
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
+        result = 0
+        nums = set(nums)
 
-        nums = sorted(set(nums))
-        result = 1
-        length = 1
-
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i - 1] + 1:
-                length += 1
-            else:
+        for n in nums:
+            if n - 1 not in nums:
+                length = 0
+                while n in nums:
+                    length += 1
+                    n += 1
                 result = max(result, length)
-                length = 1
 
-        return max(result, length)
+        return result
 
 
 Solution().longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1])
