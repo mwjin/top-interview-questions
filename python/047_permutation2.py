dcbcdd6c1a4f021704326c6ce52c5c744e2fa1e5
Minwoo Jeong <jeongmwj@gmail.com>
2022-10-27 10:03:10 +0900

    Solve `047. Permutation II ` with DFS

--- python/047_permutation2.py
+++ python/047_permutation2.py
@@ -1,10 +1,26 @@
-import itertools
-from typing import List
+from typing import Counter, List
 
 
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        return list(set(itertools.permutations(nums)))
+        result = []
+        numCnt = Counter(nums)
+
+        def dfs(path=[]):
+            if len(path) == len(nums):
+                result.append(list(path))
+                return
+
+            for n in numCnt.keys():
+                if numCnt[n] > 0:
+                    path.append(n)
+                    numCnt[n] -= 1
+                    dfs(path)
+                    path.pop()
+                    numCnt[n] += 1
+
+        dfs()
+        return result
 
 
-print(Solution().permute([1, 1, 2]))
+print(Solution().permuteUnique([1, 1, 2]))
