a3d35830e75a5d80a7c9f878ad622f96538d47a4
Minwoo Jeong <jeongmwj@gmail.com>
2022-11-07 09:29:36 +0900

    Solve with iterative approach

--- python/150_evaluate_reverse_polish_notation.py
+++ python/150_evaluate_reverse_polish_notation.py
@@ -4,23 +4,34 @@ import re
 
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        token = tokens.pop()
-        if re.match("[-+]?\d+$", token):
-            return int(token)
-        else:
-            right = self.evalRPN(tokens)
-            left = self.evalRPN(tokens)
+        def is_num(token: str):
+            return bool(re.match("[-+]?\d+$", token))
 
-            if token == "+":
-                return left + right
-            elif token == "-":
-                return left - right
-            elif token == "*":
-                return left * right
+        stack = []
+
+        while tokens:
+            token = tokens.pop()
+            if is_num(token):
+                result = int(token)
+                while stack and type(stack[-1]) == int:
+                    operand = stack.pop()
+                    operator = stack.pop()
+
+                    if operator == "+":
+                        result += operand
+                    elif operator == "-":
+                        result -= operand
+                    elif operator == "*":
+                        result *= operand
+                    else:
+                        result = int(result / operand)
+                stack.append(result)
             else:
-                return int(left / right)
+                stack.append(token)
+
+        return stack[-1]
 
 
-# print(Solution().evalRPN(["2", "1", "+", "3", "*"]))
-# print(Solution().evalRPN(["4", "13", "5", "/", "+"]))
+print(Solution().evalRPN(["2", "1", "+", "3", "*"]))
+print(Solution().evalRPN(["4", "13", "5", "/", "+"]))
 print(Solution().evalRPN(["10", "6", "9", "3", "+", "-11", "*", "/", "*"]))
