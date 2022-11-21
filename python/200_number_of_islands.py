f8fc7960d08ad4e42d06b44bb97e70cebf2ca51b
Minwoo Jeong <jeongmwj@gmail.com>
2022-11-21 09:45:24 +0900

    Solve with iterative DFS

--- python/200_number_of_islands.py
+++ python/200_number_of_islands.py
@@ -4,22 +4,25 @@ from typing import List
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         result = 0
+        visited = []
 
-        def traverse_land(row_idx, col_idx):
+        def append_land(row_idx, col_idx):
             grid[row_idx][col_idx] = "0"
             if row_idx > 0 and grid[row_idx - 1][col_idx] == "1":
-                traverse_land(row_idx - 1, col_idx)
+                visited.append((row_idx - 1, col_idx))
             if row_idx < len(grid) - 1 and grid[row_idx + 1][col_idx] == "1":
-                traverse_land(row_idx + 1, col_idx)
+                visited.append((row_idx + 1, col_idx))
             if col_idx > 0 and grid[row_idx][col_idx - 1] == "1":
-                traverse_land(row_idx, col_idx - 1)
+                visited.append((row_idx, col_idx - 1))
             if col_idx < len(grid[0]) - 1 and grid[row_idx][col_idx + 1] == "1":
-                traverse_land(row_idx, col_idx + 1)
+                visited.append((row_idx, col_idx + 1))
 
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == "1":
                     result += 1
-                    traverse_land(i, j)
+                    visited.append((i, j))
+                    while visited:
+                        append_land(*visited.pop())
 
         return result
