56874409f3e8b044c29321f42fe438d7ac244595
Minwoo Jeong <jeongmwj@gmail.com>
2022-11-30 09:42:22 +0900

    Solve with quickselect
    Slower than Python's internal sort algorithm

--- python/215_kth_largest_element_in_an_array.py
+++ python/215_kth_largest_element_in_an_array.py
@@ -1,6 +1,31 @@
 from typing import List
+from random import choice
 
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        return sorted(nums, reverse=True)[k - 1]
+        def quickSelect(nums: List[int], k: int) -> int:
+            if len(nums) == 1:
+                return nums[0]
+
+            pivot = choice(nums)
+            greater = []
+            equal = []
+            less = []
+
+            for n in nums:
+                if n > pivot:
+                    greater.append(n)
+                elif n == pivot:
+                    equal.append(n)
+                else:
+                    less.append(n)
+
+            if len(greater) >= k:
+                return quickSelect(greater, k)
+            elif len(greater) + len(equal) >= k:
+                return pivot
+            else:
+                return quickSelect(less, k - len(greater) - len(equal))
+
+        return quickSelect(nums, k)
