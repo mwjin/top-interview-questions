9feec268a9d2d15192a06e537ee69a066d4747cc
Minwoo Jeong <jeongmwj@gmail.com>
2022-12-13 09:49:43 +0900

    Improve the memory and performance with deque

--- python/239_sliding_window_maximum.py
+++ python/239_sliding_window_maximum.py
@@ -1,24 +1,20 @@
 from typing import List
-import heapq
+from collections import deque
 
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        if k == 1:
-            return nums
-
-        num_heap = []
-        for i in range(k - 1):
-            heapq.heappush(num_heap, (-nums[i], i))
-
         result = []
-        for i in range(len(nums) - k + 1):
-            heapq.heappush(num_heap, (-nums[i + k - 1], i + k - 1))
+        window = deque()
 
-            while num_heap[0][1] < i:
-                heapq.heappop(num_heap)
-
-            result.append(-num_heap[0][0])
+        for i, n in enumerate(nums):
+            while window and nums[window[-1]] <= n:
+                window.pop()
+            while window and i - window[0] >= k:
+                window.popleft()
+            window.append(i)
+            if i >= k - 1:
+                result.append(nums[window[0]])
 
         return result
 
