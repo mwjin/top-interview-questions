72cb0b0fb95104589fd2d2846dbcc7239f4458d2
Minwoo Jeong <jeongmwj@gmail.com>
2022-12-14 10:04:56 +0900

    Reduce the number of search

--- python/240_search_a_2d_matrix_2.py
+++ python/240_search_a_2d_matrix_2.py
@@ -3,23 +3,33 @@ from typing import List
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        def binarySearch(row: List[int], target: int) -> bool:
+        low = 0
+        high = len(matrix[0]) - 1
+
+        while low <= high:
+            mid = low + (high - low) // 2
+            if matrix[0][mid] == target:
+                return True
+            elif matrix[0][mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        start = min(low, high)
+        if start < 0:
+            return False
+
+        for col_idx in range(start, -1, -1):
             low = 0
-            high = len(row) - 1
+            high = len(matrix) - 1
 
             while low <= high:
                 mid = low + (high - low) // 2
-                if row[mid] == target:
+                if matrix[mid][col_idx] == target:
                     return True
-                elif row[mid] < target:
+                elif matrix[mid][col_idx] < target:
                     low = mid + 1
                 else:
                     high = mid - 1
-
-            return False
-
-        for row in matrix:
-            if binarySearch(row, target):
-                return True
 
         return False
