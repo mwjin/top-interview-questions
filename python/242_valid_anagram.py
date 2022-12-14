b4cf7ebe370ad3049a6246751da8f88c89cdf456
Minwoo Jeong <jeongmwj@gmail.com>
2022-12-15 08:57:39 +0900

    Solve with the other approach
    Use dictionaries for counting

--- python/242_valid_anagram.py
+++ python/242_valid_anagram.py
@@ -1,3 +1,14 @@
+from collections import defaultdict
+
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
+
+        counter = defaultdict(int)
+        for i in range(len(s)):
+            counter[s[i]] += 1
+            counter[t[i]] -= 1
+
+        return not any(counter.values())
