18876a37bb60c12a09b187907664da988d23c006
Minwoo Jeong <jeongmwj@gmail.com>
2022-12-21 10:42:40 +0900

    Solve with extra constant space

--- python/287_find_the_duplicate_number.py
+++ python/287_find_the_duplicate_number.py
@@ -3,8 +3,16 @@ from typing import List
 
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        memory = set()
-        for n in nums:
-            if n in memory:
-                return n
-            memory.add(n)
+        fast = nums[nums[0]]
+        slow = nums[0]
+
+        while fast != slow:
+            fast = nums[nums[fast]]
+            slow = nums[slow]
+
+        slow = 0
+        while fast != slow:
+            fast = nums[fast]
+            slow = nums[slow]
+
+        return slow
