81534a05de45cb1bde27369daf0d40a3f503a070
Minwoo Jeong <jeongmwj@gmail.com>
2023-01-02 09:37:40 +0900

    Solve without loop or recursion

--- python/326_power_of_three.py
+++ python/326_power_of_three.py
@@ -1,11 +1,3 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        if n <= 0:
-            return False
-
-        power = 1
-
-        while power < n:
-            power *= 3
-
-        return power == n
+        return n > 0 and 3**19 % n == 0
