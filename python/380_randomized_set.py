96b45b77eaf4391ae57e7729eb8058f3dc06c411
Minwoo Jeong <jeongmwj@gmail.com>
2023-01-13 09:20:08 +0900

    Improve performance by lazily creating the list

--- python/380_randomized_set.py
+++ python/380_randomized_set.py
@@ -4,21 +4,29 @@ import random
 class RandomizedSet:
     def __init__(self):
         self._set = set()
+        self._list = []
+        self._changed = False
 
     def insert(self, val: int) -> bool:
         if val in self._set:
             return False
         self._set.add(val)
+        self._changed = True
         return True
 
     def remove(self, val: int) -> bool:
         if val in self._set:
             self._set.remove(val)
+            self._changed = True
             return True
         return False
 
     def getRandom(self) -> int:
-        return random.choice(list(self._set))  # Not O(1)!
+        if self._changed:
+            self._list.clear()
+            self._list.extend(self._set)
+            self._changed = False
+        return random.choice(self._list)
 
 
 # Your RandomizedSet object will be instantiated and called as such:
