3e3a1f45e957b5fe9d7e383b0b7c2aafc9d4ccd2
Minwoo Jeong <jeongmwj@gmail.com>
2023-01-17 17:00:49 +0900

    Solve without a hash table

--- python/395_longest_substring_with_at_least_k_repeating_characters.py
+++ python/395_longest_substring_with_at_least_k_repeating_characters.py
@@ -1,11 +1,8 @@
-from collections import Counter
-
-
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
-        counter = Counter(s)
-        for c in counter:
-            if counter[c] < k:
+        charset = set(s)
+        for c in charset:
+            if s.count(c) < k:
                 return max(
                     self.longestSubstring(substr, k) for substr in s.split(c)
                 )
