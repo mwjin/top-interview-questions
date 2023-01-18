77e51eec553666aa60f2f7fa6047bd3745a72fe6
Minwoo Jeong <jeongmwj@gmail.com>
2023-01-18 10:05:08 +0900

    Improve the solution

--- python/412_fizz_buzz.py
+++ python/412_fizz_buzz.py
@@ -6,13 +6,12 @@ class Solution:
         result = []
 
         for i in range(1, n + 1):
-            if i % 15 == 0:
-                result.append("FizzBuzz")
-            elif i % 5 == 0:
-                result.append("Buzz")
-            elif i % 3 == 0:
-                result.append("Fizz")
-            else:
-                result.append(f"{i}")
+            s = ""
+            if i % 3 == 0:
+                s += "Fizz"
+            if i % 5 == 0:
+                s += "Buzz"
+
+            result.append(s if s else str(i))
 
         return result
