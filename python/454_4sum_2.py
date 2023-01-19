f8d66cd2a1735a30f1eb73ffe9e8f133e4a5e3f3
Minwoo Jeong <jeongmwj@gmail.com>
2023-01-19 14:06:53 +0900

    Use only one hash table

--- python/454_4sum_2.py
+++ python/454_4sum_2.py
@@ -10,18 +10,16 @@ class Solution:
         nums3: List[int],
         nums4: List[int],
     ) -> int:
-        sum_dict1 = self.getSums(nums1, nums2)
-        sum_dict2 = self.getSums(nums3, nums4)
+        freq = defaultdict(int)
 
-        result = 0
-        for n, cnt in sum_dict2.items():
-            result += cnt * sum_dict1[-n]
-
-        return result
-
-    def getSums(self, nums1: List[int], nums2: List[int]) -> dict:
-        result = defaultdict(int)
         for n1 in nums1:
             for n2 in nums2:
-                result[n1 + n2] += 1
+                freq[n1 + n2] += 1
+
+        result = 0
+
+        for n3 in nums3:
+            for n4 in nums4:
+                result += freq[-(n3 + n4)]
+
         return result
